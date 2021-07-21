d73a47c45b8238fb21ee6f2316229fcd47f7a6f5
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-07-21 18:20:41 -0300

    Creating parking controller

--- src/controllers/parkings.js
+++ src/controllers/parkings.js
@@ -1,10 +1,23 @@
-const axios = require('axios');
+const {makeRequest} = require('../utils/apiRequest.js');
+const {mapper, filter} = require('../utils/dataHandlers.js');
 
 const getParkings = async (req, res, next) => {
-	res
-		.send({response: 'hola', message: 'Success'})
+	const location = req.query.location;
+	const score = Number(req.query.score) || 2.5;
 
-		.catch((err) => next(err));
+	try {
+		if (!location) {
+			throw new Error();
+		}
+		const {data} = await makeRequest(location);
+		const businessesInfo = mapper(data.businesses);
+		const filteredBusinesses = filter(businessesInfo, score);
+
+		res.send({response: filteredBusinesses, message: 'Success'});
+	} catch (err) {
+		err.response ? console.log(err.response.data) : console.log(err);
+		next(err);
+	}
 };
 
 module.exports = {
